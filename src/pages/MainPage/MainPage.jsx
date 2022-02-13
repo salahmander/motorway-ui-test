9d2f7203129e5e24da4956f9d4ba96ed2505b37a
salahmander <salahabdo@hotmail.co.uk>
2022-02-13 12:56:03 +0000

    Initial setup.

---
+++ src/pages/MainPage/MainPage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+// Styling
+import "./MainPage.css";
+
+const HomePage = () => {
+
+  return (
+    <main className="main-page-container">
+      <h1>Main Page</h1>
+    </main>
+  );
+};
+
+export default HomePage;
