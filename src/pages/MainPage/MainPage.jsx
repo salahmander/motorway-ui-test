096e2ebb52d98756b2ef30479d2ce18567a12d43
salahmander <salahabdo@hotmail.co.uk>
2022-02-13 13:01:15 +0000

    Initial structure for the main the page.

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -3,11 +3,22 @@ import React from "react";
 // Styling
 import "./MainPage.css";
 
-const HomePage = () => {
-
+const HomePage = ({ images }) => {
   return (
     <main className="main-page-container">
-      <h1>Main Page</h1>
+      <section className="main-top-container">
+        <div className="main-top-left-image-container">
+          <h1>Random Image</h1>
+        </div>
+        <div className="main-top-right-form-container">
+          <div className="form-container">
+            <h1>Form Component</h1>
+          </div>
+        </div>
+      </section>
+      <section className="main-bottom-container">
+        <h1>Gallery Component</h1>
+      </section>
     </main>
   );
 };
